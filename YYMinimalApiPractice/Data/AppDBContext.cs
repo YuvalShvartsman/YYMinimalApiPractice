262ad14b48b4cc99b7c85c000beae12a46fbcfa1
Yuval <yuval.shvartsman@gmail.com>
2024-10-09 08:24:07 +0300

    Created a DBContext

---
+++ YYMinimalApiPractice/Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using YYMinimalApiPractice.Models;
+
+namespace YYMinimalApiPractice.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<UserModel> User { get; set; }
+        public DbSet<TodoModel> Todo { get; set; }
+
+    }
+}
