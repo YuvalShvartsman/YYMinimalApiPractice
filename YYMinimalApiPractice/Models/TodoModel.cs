7472cd2dd03e55f9095f928481b8faf47b387462
YuvalShvartsman <yuval.shvartsman@gmail.com>
2024-10-09 08:29:32 +0300

    Added Data annotations to my models.

--- YYMinimalApiPractice/Models/TodoModel.cs
+++ YYMinimalApiPractice/Models/TodoModel.cs
@@ -1,9 +1,14 @@
-﻿namespace YYMinimalApiPractice.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YYMinimalApiPractice.Models
 {
     public class TodoModel
     {
+        [Key]
         public required string Id { get; set; }
+        [Required]
         public required string Title { get; set; }
+        [Required]
         public required bool IsCompleted { get; set; }
     }
 }
--- YYMinimalApiPractice/Models/UserModel.cs
+++ YYMinimalApiPractice/Models/UserModel.cs
@@ -1,10 +1,14 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace YYMinimalApiPractice.Models
 {
     public record UserModel
     {
+        [Key]
         public required string Id { get; set; }
+        [Required]
         public required string Name { get; set; }
 
     }
